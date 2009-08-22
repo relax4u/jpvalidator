71c02d209c7fec285aa196f2e413b7ecead1b72f
relax4u <wanderers.room@gmail.com>
2009-08-22 17:50:02 +0900

    fixed require path

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
-require File.dirname(__FILE__) + "lib/core_ext/jp_blank"
-require File.dirname(__FILE__) + "lib/active_record/errors"
-require File.dirname(__FILE__) + "lib/active_record/jpvalidator"
+require File.dirname(__FILE__) + "/lib/core_ext/jp_blank"
+require File.dirname(__FILE__) + "/lib/active_record/errors"
+require File.dirname(__FILE__) + "/lib/active_record/jpvalidator"
 
 ActiveRecord::Base.send :include, ActiveRecord::Jpvalidator
 
